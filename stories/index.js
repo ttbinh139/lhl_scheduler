db1ad09ec6382c2249f0b6039ace934e5e9866e9
Binh Trinh <ttbinh139@gmail.com>
2022-06-15 21:46:23 -0700

    Props and states of form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -151,7 +152,9 @@ storiesOf("Appointment", module)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
   .add("Confirm", () => <Confirm message="Do you want to delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", () => <Status message="Deleting!!!"/>)
-  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>);
+  .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
+  .add("Edit", () => <Form student="Binh" interviewer={3} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />);
 
 
 
