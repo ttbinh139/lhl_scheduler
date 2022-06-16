965bed365dda16386edf1368d6cfe909bcc44c34
Binh Trinh <ttbinh139@gmail.com>
2022-06-15 20:50:51 -0700

    Implement Appoitment components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "components/Appointment/styles.scss";
+
+function Appointment(props) {
+  return (
+    <article className="appointment"></article>
+  );
+};
+
+export default Appointment;
