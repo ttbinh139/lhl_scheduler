c15993c403adfc0cb08aa12fcad3c73cc92a32c2
Binh Trinh <ttbinh139@gmail.com>
2022-06-17 18:25:11 -0700

    Applied custom hook to Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,6 +6,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview } from "helpers/selectors.js";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Application(props) {
   //const [days, setDays] = useState([]);
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,17 +2,35 @@ import React from "react";
 
 
 import "components/Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
 
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
-      {!props.interview && <Empty />}
+     {/*  {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
+      {!props.interview && <Empty />} */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form onCancel={() => back()} interviewers={[]} />}
     </article>
   );
 };
